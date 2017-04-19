21c9c902451fdd763e789936bfe318cae961d810
anerik <nicorezzano1@gmail.com>
2017-04-18 23:01:56 -0300

    bringup 3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,7 +129,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+# BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
